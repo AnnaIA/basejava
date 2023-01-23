9836a85de34f58088ffce556828dc4645bc60a06
ann.iljichova@yandex.ru <fakechild@list.ru>
2023-01-23 23:47:16 +0300

    added implementation for ArrayStirage.class

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,28 +3,60 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    static int defaultSize = 10000;
+    int pointer = 0;
 
     void clear() {
+        for (int i = 0; i < pointer+1; i++) {
+            if (storage[i] == null){
+                break;
+            }
+            storage[i] = null;
+        }
+        pointer = 0;
     }
 
     void save(Resume r) {
+        storage[pointer] = r;
+        pointer++;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < pointer; i++) {
+            if(storage[i].uuid.equals(uuid))
+                return storage[i];
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i <= pointer; i++) {
+            if(storage[i].uuid.equals(uuid)){
+                for (int j = i; j < pointer+1; j++) {
+                    if(storage[j+1] != null) {
+                        storage[j] = storage[j + 1];
+                    }
+                }
+                pointer--;
+                storage[pointer] = null;
+                break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] resume = new Resume[pointer];
+        for (int i = 0; i < pointer; i++) {
+            resume[i] = storage[i];
+        }
+        //System.arraycopy(storage, 0, resume, 0, pointer); The same function may be like that.
+        return resume;
     }
 
     int size() {
-        return 0;
+        return pointer;
     }
 }
